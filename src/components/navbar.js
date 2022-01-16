dc6a4561b6d9a4d1fd39fba6b0987bb692705e34
Jacob Cavazos <jjcav84@gmail.com>
2022-01-16 02:25:27 -0600

    patched-nav component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 import {
   MDBContainer,
   MDBNavbar,
-  MDBNavbarNav,
-  MDBNavItem,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBIcon,
   Animation,
 } from 'mdbreact'
 import { Link } from 'gatsby'
@@ -30,38 +25,6 @@ class NavbarPage extends Component {
               <Logo />
             </Link>
           </Animation>
-          <MDBNavbarToggler
-            name="navbar-toggler"
-            onClick={this.toggleCollapse}
-          />
-          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-            <MDBNavbarNav left>
-              <MDBNavItem className="pt-2 text">
-                <a
-                  className="white-text"
-                  href="https://portfolio.visualcloudfx.com"
-                >
-                  Design Portfolio
-                </a>
-              </MDBNavItem>
-            </MDBNavbarNav>
-            <MDBNavbarNav right>
-              <div className="d-flex align-items-center">
-                <a
-                  className="white-text"
-                  href="https://facebook.com/visualcloudfx/"
-                >
-                  <MDBIcon fab icon="facebook" className="ml-1" />
-                </a>
-                {/*  <CustomNavLink to="#!">
-                  <MDBIcon fab icon="twitter" className="ml-1" />
-                </CustomNavLink>
-                <CustomNavLink to="#!">
-                  <MDBIcon fab icon="linkedin" className="ml-1" />
-                </CustomNavLink> */}
-              </div>
-            </MDBNavbarNav>
-          </MDBCollapse>
         </MDBContainer>
       </MDBNavbar>
     )
